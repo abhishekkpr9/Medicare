8d47022d90778cb801bb931efadbe630e085597d
Abhishek Kapoor <abhishekkpr9@gmail.com>
2018-12-17 02:39:23 +0530

    UI changes

--- Medicare/App_Start/BundleConfig.cs
+++ Medicare/App_Start/BundleConfig.cs
@@ -26,6 +26,12 @@ namespace Medicare
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/dashboard/css").Include(
+                      "~/Content/assets/css/*.css"));
+
+            bundles.Add(new ScriptBundle("~/Script/dashboard").Include(
+                      "~/Scripts/*.js"));
         }
     }
 }
---
+++ Medicare/Areas/Dashboard/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Medicare.Areas.Dashboard.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Dashboard/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
